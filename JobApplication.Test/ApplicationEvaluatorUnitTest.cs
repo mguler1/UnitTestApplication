494cb99e24f7af036e64b37807e632fc12b656f4
Muhammed Mehti GÜLER <mehti.guler@gmail.com>
2024-07-30 22:47:15 +0300

    Testing Be Continued

--- JobApplication.Test/ApplicationEvaluatorUnitTest.cs
+++ JobApplication.Test/ApplicationEvaluatorUnitTest.cs
@@ -21,5 +21,44 @@ namespace JobApplication.Test
 
             Assert.AreEqual(appResult,ApplicationResult.AutoRejected);
         }
+
+
+        [Test]
+        public void Application_WithNoTechStack_TransferredAutoRejected()
+        {
+            var evaluater = new ApplicationEvaluators();
+
+            var form = new JApplication()
+            {
+                Applicant = new Applicant(),
+                 TechStackList=new System.Collections.Generic.List<string>() { ""}
+            };
+
+            var appResult = evaluater.Evaluate(form);
+
+            Assert.AreEqual(appResult, ApplicationResult.AutoRejected);
+        }
+
+
+
+        [Test]
+        public void Application_WithTechStackOver75P_TransferredAutoAccepted()
+        {
+            var evaluater = new ApplicationEvaluators();
+
+            var form = new JApplication()
+            {
+                Applicant = new Applicant() { Age = 19 },
+                TechStackList = new System.Collections.Generic.List<string>() {"C#","Java","Go" },
+                YearsOfExprience=16
+
+            };
+
+            var appResult = evaluater.Evaluate(form);
+
+            Assert.AreEqual(appResult, ApplicationResult.AutoAccepted);
+        }
+
+
     }
 }
--- JobApplication/ApplicationEvaluators.cs
+++ JobApplication/ApplicationEvaluators.cs
@@ -9,6 +9,7 @@ namespace JobApplication
     public class ApplicationEvaluators
     {
         private const int minAge = 18;
+        private const int autoAcceptYearsofExperience = 15;
         private  List<string> teckStackList = new() {"C#","Java","Go" };
 
         public ApplicationResult Evaluate(JApplication form)
@@ -17,14 +18,29 @@ namespace JobApplication
             {
                 return ApplicationResult.AutoRejected;
             }
-            return ApplicationResult.AutoCompleted;
+            var sr = GetTechStackSimilarityRate(form.TechStackList);
+            if (sr<25)
+            {
+                return ApplicationResult.AutoRejected;
+            }
+            if (sr>75 && form.YearsOfExprience >= autoAcceptYearsofExperience)
+            { 
+                return ApplicationResult.AutoAccepted;
+            }
+            return ApplicationResult.AutoAccepted;
+        }
+
+        private int GetTechStackSimilarityRate(List<string> teckStackList) 
+        { 
+            var matchCount=teckStackList.Where(x=>teckStackList.Contains(x,StringComparer.OrdinalIgnoreCase)).Count();
+            return (int)((double)matchCount/teckStackList.Count)*100;
         }
     }
 }
 public enum ApplicationResult
 {
     AutoRejected,
-    AutoCompleted,
+    AutoAccepted,
     TransferredToHr,
     TransferredToLead,
     TransferredToCto
